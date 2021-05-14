59a5f66a61e0685877b38050e8da1320498cf937
xinyuhao28 <xinyu_hao@126.com>
2021-05-14 17:21:40 +0800

    test

---
+++ src/main/java/com/javabase/base/controler/Hello.java
@@ -0,0 +1,40 @@
+package com.javabase.base.controler;
+
+import com.javabase.base.logic.DemoThread;
+import com.javabase.base.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Controller
+public class Hello {
+    @Autowired
+    DemoThread dt;
+
+    @RequestMapping("/index")
+    public String sayHello() {
+/*
+
+        ArrayList<User> list;
+        Map<Integer, User> map ;
+        ThreadPoolExecutor
+        Collection
+*/
+
+
+        dt.start();
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e ){
+            System.out.println("e = " + e);
+
+        }
+
+        return "index";
+    }
+
+}
